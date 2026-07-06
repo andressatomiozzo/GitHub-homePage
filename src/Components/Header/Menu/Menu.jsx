2be7a74df418721c49ac0965f1b8f4b0cc8cb8e4
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-07-06 17:39:39 -0400

    Estilizei o memu hamburguer para que fique como um modal

--- src/Components/Header/Menu/Menu.jsx
+++ src/Components/Header/Menu/Menu.jsx
@@ -21,59 +21,65 @@ import MCPRegistry from "../../../assets/svg/mcp_registry.svg?react";
 
 const Menu = () => {
   const [toggleMenu, setToggleMenu] = React.useState(false);
+
+  const handleOutideClick = (event) => {
+    if(event.target === event.currentTarget) setToggleMenu(false)
+  }
   return (
     <>
       <Button1 type="button1" className={styles.buttonHamburguer} onClick={() => setToggleMenu(true)}>
         <MenuIcon />
       </Button1>
       {toggleMenu && (
-        <aside className={`${styles.aside} animeLeft`}>
-          <div className={styles.header}>
-            <LogoIcon className={styles.logo}/>
-            <Button1 type="button2" aria-label="Close Menu" data-tooltip="Close Menu" onClick={() => setToggleMenu(false)}>
-              <CloseMenu />
-            </Button1>
-          </div>
-          <ul className={styles.lista}>
-            <li>
-              <ul className={styles.listaGrupo}>
-                <li>
-                  <FeatureItem svg={Home} title="Home" className={styles.active}/>
-                </li>
-                <li>
-                  <FeatureItem svg={AllIssues} title="All issues" />
-                </li>
-                <li>
-                  <FeatureItem svg={AllPullRequest} title="All pull requests" />
-                </li>
-                <li>
-                  <FeatureItem svg={AllRepositories} title="All repositories" />
-                </li>
-                <li>
-                  <FeatureItem svg={Projects} title="Projects" />
-                </li>
-                <li>
-                  <FeatureItem svg={Discussions} title="Discussions" />
-                </li>
-                <li>
-                  <FeatureItem svg={Codespaces} title="Codespaces" />
-                </li>
-                <li>
-                  <FeatureItem svg={Copilot} title="Copilot" />
-                </li>
-              </ul>
-            </li>
-            <li className={styles.divisor}></li>
-            <li>
-              <ul className={styles.listaGrupo}>
-                <li><FeatureItem svg={Explore} title="Explore" /></li>
-                <li><FeatureItem svg={MarketPlace} title="MarketPlace" /></li>
-                <li><FeatureItem svg={MCPRegistry} title="MCP registry" /></li>
-              </ul>
-            </li>
-            <li className={styles.divisor}></li>
-          </ul>
-        </aside>
+        <div className={styles.modal} onClick={handleOutideClick}>
+          <aside className={`${styles.aside} animeLeft`}>
+            <div className={styles.header}>
+              <LogoIcon className={styles.logo}/>
+              <Button1 type="button2" aria-label="Close Menu" data-tooltip="Close Menu" onClick={() => setToggleMenu(false)}>
+                <CloseMenu />
+              </Button1>
+            </div>
+            <ul className={styles.lista}>
+              <li>
+                <ul className={styles.listaGrupo}>
+                  <li>
+                    <FeatureItem svg={Home} title="Home" className={styles.active}/>
+                  </li>
+                  <li>
+                    <FeatureItem svg={AllIssues} title="All issues" />
+                  </li>
+                  <li>
+                    <FeatureItem svg={AllPullRequest} title="All pull requests" />
+                  </li>
+                  <li>
+                    <FeatureItem svg={AllRepositories} title="All repositories" />
+                  </li>
+                  <li>
+                    <FeatureItem svg={Projects} title="Projects" />
+                  </li>
+                  <li>
+                    <FeatureItem svg={Discussions} title="Discussions" />
+                  </li>
+                  <li>
+                    <FeatureItem svg={Codespaces} title="Codespaces" />
+                  </li>
+                  <li>
+                    <FeatureItem svg={Copilot} title="Copilot" />
+                  </li>
+                </ul>
+              </li>
+              <li className={styles.divisor}></li>
+              <li>
+                <ul className={styles.listaGrupo}>
+                  <li><FeatureItem svg={Explore} title="Explore" /></li>
+                  <li><FeatureItem svg={MarketPlace} title="MarketPlace" /></li>
+                  <li><FeatureItem svg={MCPRegistry} title="MCP registry" /></li>
+                </ul>
+              </li>
+              <li className={styles.divisor}></li>
+            </ul>
+          </aside>
+        </div>
       )}
     </>
   );
